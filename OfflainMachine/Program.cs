109bb7097ab4519b27bf69607be9daed37008c0c
dr.lis <dr.lis@outlook.com>
2023-06-29 01:47:03 +0300

    Add project files.

---
+++ OfflainMachine/Program.cs
@@ -0,0 +1,36 @@
+﻿using Nethereum.Signer;
+using System;
+using Nethereum.Web3;
+using System.IO;
+using Nethereum.Web3.Accounts;
+
+namespace OfflineMachine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StreamReader stream1 = new StreamReader("../../../../OnlineMachine/bin/Debug/net5.0/transaction_in.txt");
+			String from = stream1.ReadLine();
+			String to = stream1.ReadLine();
+			String nonce = stream1.ReadLine();
+			String value = stream1.ReadLine();
+			stream1.Close();
+
+			Console.WriteLine(from);
+			Console.WriteLine(to);
+			Console.WriteLine(nonce);
+			Console.WriteLine(value);
+
+			//String tx = args[0];
+			var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
+			var signer = new EthereumMessageSigner();
+			String message = $"{from} {to} {nonce} {value}";
+			String signature = signer.EncodeUTF8AndSign(message, new EthECKey(privateKey));
+
+			StreamWriter stream2 = new StreamWriter("./transaction_out.txt");
+			stream2.WriteLine(signature);
+			stream2.Close();
+		}
+	}
+}
---
+++ OnlineMachine/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Nethereum.Accounts;
+using Nethereum.BlockchainProcessing.BlockStorage.Entities;
+using System.IO;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.NonceServices;
+using Nethereum.Web3;
+
+namespace OnlineMachine
+{
+	class Program
+	{
+		public static String apiToken = "d88243da67d24bd29963d6c6b2972a8b";
+
+		/// <summary>
+		/// from, to, value => file, which contains unsigned tx
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			String endpoint = $"https://goerli.infura.io/v3/{apiToken}";
+			var web3 = new Web3(endpoint);
+
+			String from = args[0];
+			String to = args[1];
+			UInt64 value = Convert.ToUInt64(args[2]);
+
+			var nonceService = new InMemoryNonceService(from, web3.Client);
+			var nonceRequest = web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(from);
+			nonceRequest.Wait();
+			var nonceRequest1 = nonceService.GetNextNonceAsync();
+			nonceRequest1.Wait();
+			var nonce = nonceRequest1.Result;
+
+			StreamWriter writer = new StreamWriter("./transaction_in.txt");
+			writer.WriteLine(from);
+			writer.WriteLine(to);
+			writer.WriteLine(nonce);
+			writer.WriteLine(value);
+			writer.Close();
+
+			Console.WriteLine("Please run procces at offline machine and press any key");
+			Console.ReadKey();
+
+			StreamReader streamReader = new StreamReader("../../../../OfflineMachine/bin/Debug/net5.0/transaction_out.txt");
+			String signature = streamReader.ReadLine();
+			streamReader.Close();
+
+			Console.WriteLine(signature);
+		}
+	}
+}
